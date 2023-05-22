b3ad04146d5ffd65752fbd5074cd430d7f0eb5ec
dibot55 <diebots555@gmail.com>
2023-05-22 14:16:39 -0600

    Recibir Parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common'; // Decoradores
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,5 +19,34 @@ export class AppController {
   @Get('new')
   newEndpoint() {
     return 'Este soy yo pero en otro endpoint';
+  }
+
+  //Recibir un unico parametro
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `this is the ${id}`;
+  }
+
+  //Recibir dos parametros
+  @Get('categories/:cateogoriesId/products/:productsId')
+  getCategories(
+    @Param('cateogoriesId') cateogoriesId: string,
+    @Param('productsId') productsId: string,
+  ) {
+    return `this is the ${cateogoriesId} and the produts is ${productsId}`;
+  }
+
+  // Validar un unico tipo de parametro
+  @Get('prod/:id')
+  getProd(
+    @Param(
+      'id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    id: number,
+  ) {
+    return `this is the ${id}`;
   }
 }
